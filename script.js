ef4200ffcef3084098bd6c62ca15581b02456de6
Rado <sitcom93@abv.bg>
2025-10-23 00:19:11 +0300

    Fix bugs in the script.js and test.html
    Fix bugs in the script.js and test.html
    
    Fix bugs in the script.js and test.html
    
    Fix bugs in the script.js and test.html

--- script.js
+++ script.js
@@ -24,7 +24,31 @@ function calculateAndDisplay(fn) {
   document.getElementById('result').textContent = fn(a, b);
 }
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 ['add', 'subtract', 'multiply'].forEach(op => {
   document.getElementById(`btn-${op}`)
     .addEventListener('click', () => calculateAndDisplay(window[op]));
 });
+<<<<<<< HEAD
+
+=======
+=======
+>>>>>>> be24f12 (Fix bugs in the script.js and test.html)
+=======
+>>>>>>> 1d5f6c9 (Fix bugs in the script.js and test.html)
+document.getElementById('btn-add')
+  .addEventListener('click', () => calculateAndDisplay(add));
+
+document.getElementById('btn-subtract')
+  .addEventListener('click', () => calculateAndDisplay(subtract));
+
+document.getElementById('btn-multiply')
+  .addEventListener('click', () => calculateAndDisplay(multiply));
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1d5f6c9 (Fix bugs in the script.js and test.html)
+>>>>>>> be24f12 (Fix bugs in the script.js and test.html)
+=======
+>>>>>>> 1d5f6c9 (Fix bugs in the script.js and test.html)
